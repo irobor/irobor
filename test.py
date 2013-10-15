8e1dced95869030e30dde5c4e15276b6dcb3cba0
irobor <lion.vvm@gmail.com>
2013-10-15 17:21:27 +0400

    Учебное пособие

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+def foo(arg1, arg2 = 0):
+    print ('Аргумент функции равен:')
+    print (arg1[::3])
+a = [1,2,3,4,5]
+foo(a)
+try:
+    print ('Привет',sys.argv[1])
+except:
+    print('Извините ошибка')
+	
+input()
